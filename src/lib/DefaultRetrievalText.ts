3251c190c3df0ff39bd234122457c63594469c04
bossthanawat <boss41680@gmail.com>
2024-02-25 15:11:01 +0700

    Update DefaultRetrievalText with personal information and education details

--- src/lib/DefaultRetrievalText.ts
+++ src/lib/DefaultRetrievalText.ts
@@ -1,11 +1,18 @@
-export const DefaultRetrievalText = `# Hi I'm Boss Thanawat
-Software Engineer and Front-End Developer
+export const DefaultRetrievalText = `
+# Hi I'm Boss Thanawat
+
 Hi, my name is name is Thanawat Kittichaikarn.
-Experienced front-end developer(React) and software engineer with a focus on creating accessible web products. Proficient in React and dedicated to crafting visually appealing and user-friendly interfaces.
+I like to watch anime, my favorite story 
+- Made in Abyss
+- Sousou no Frieren
+- Botchi za Rokku!
+- and many more that I like
+I like to play games a lot. Especially fps games
+I like to take pictures a lot. I have portfoilio to take pictures too. https://bossthanawat.myportfolio.com/
 
-I work in EASY RICE DIGITAL TECHNOLOGY CO., LTD.
+I currently work at Easy Rice Digital Technology doing Front-End Developer
 
-update last year 2024
-`;
+Education KMITL
+Bachelor's Degree in Computer Engineering Department, majoring in Information Engineering.`;
 
 export default DefaultRetrievalText;

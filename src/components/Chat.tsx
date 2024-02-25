fc7a60c2af7093e5d2fb7028a61d9884ac1a5041
bossthanawat <boss41680@gmail.com>
2024-02-25 16:35:00 +0700

    Add Badge component to Chat

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -8,6 +8,7 @@ import ReactMarkdown from 'react-markdown';
 import BouncingDotsLoader from './BouncingDotsLoader';
 import Scrollbar from './Scrollbar';
 import { Input } from './ui/input';
+import { Badge } from './ui/badge';
 
 export type UnitType = "human" | "ai"
 
@@ -37,7 +38,7 @@ const Chat = (props: ChatSectionProps) => {
     },
 
   });
-  const { control, handleSubmit, reset } = methods;
+  const { control, handleSubmit, reset, setValue, formState: { isDirty } } = methods;
 
   useEffect(() => {
     endRef.current?.scrollIntoView({ behavior: 'smooth' });
@@ -45,16 +46,18 @@ const Chat = (props: ChatSectionProps) => {
 
   const handleSubmitCustom = handleSubmit((data: ValueForm) => {
     onSubmit(data);
-    reset();
+    reset({}, {
+      keepDirty: true
+    });
   })
 
   return (
     <>
       <FormProvider {...methods}>
         <form onSubmit={handleSubmitCustom}>
-          <div className="flex flex-col gap-4 p-4 ">
+          <div className="flex flex-col gap-4 p-4 min-h-[425px] justify-between">
             {chats.length > 0 ? (
-              <Scrollbar className="h-80 overflow-y-auto p-2">
+              <Scrollbar className="h-80 overflow-y-auto pr-2">
                 <div className="flex flex-col gap-2">
                   {chats.map((chat, index) => (
                     <React.Fragment key={index}>
@@ -80,7 +83,7 @@ const Chat = (props: ChatSectionProps) => {
                           <BouncingDotsLoader />
                         ) : (
                           <div className="bg-orange-100 p-3 rounded-r-xl rounded-bl-xl">
-                            <p className="text-sm"><ReactMarkdown>{chat?.content}</ReactMarkdown></p>
+                            <div className="text-sm"><ReactMarkdown>{chat?.content}</ReactMarkdown></div>
                           </div>
                         )}
                       </div>}
@@ -91,7 +94,7 @@ const Chat = (props: ChatSectionProps) => {
               </Scrollbar>
             ) : (
               <>
-                <div className="flex gap-2 items-center">
+                <div className="flex gap-2 items-start">
                   <Image
                     src={'/assets/images/notion-avatar-bot.png'}
                     alt="user"
@@ -106,33 +109,49 @@ const Chat = (props: ChatSectionProps) => {
                 </div>
               </>
             )}
-            <Controller
-              name={'message'}
-              control={control}
-              render={({ field }) => (
-                <div className="relative">
-                  <Input
-                    placeholder="Type your message"
-                    className=" pr-8"
-                    disabled={props.disabledType}
-                    {...field}
-                  />
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="1em"
-                    height="1em"
-                    viewBox="0 0 24 24"
-                    className="absolute right-2 top-1/2 -translate-y-1/2 transform cursor-pointer"
-                    onClick={handleSubmitCustom}
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M13 20h-2V8l-5.5 5.5l-1.42-1.42L12 4.16l7.92 7.92l-1.42 1.42L13 8z"
-                    ></path>
-                  </svg>
-                </div>
-              )}
-            />
+            <div>
+              {!isDirty && <div className='flex gap-2 mb-2'>
+                <Badge
+                  variant="secondary"
+                  className='cursor-pointer'
+                  onClick={() => setValue("message", "What's his name?", { shouldDirty: true })}>
+                  What&apos;s his name?
+                </Badge>
+                <Badge
+                  variant="secondary"
+                  className='cursor-pointer'
+                  onClick={() => setValue("message", "What does he like?", { shouldDirty: true })}>
+                  What does he like?
+                </Badge>
+              </div>}
+              <Controller
+                name={'message'}
+                control={control}
+                render={({ field }) => (
+                  <div className="relative justify-self-end">
+                    <Input
+                      placeholder="Type your message"
+                      className=" pr-8"
+                      disabled={props.disabledType}
+                      {...field}
+                    />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 24 24"
+                      className="absolute right-2 top-1/2 -translate-y-1/2 transform cursor-pointer"
+                      onClick={handleSubmitCustom}
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M13 20h-2V8l-5.5 5.5l-1.42-1.42L12 4.16l7.92 7.92l-1.42 1.42L13 8z"
+                      ></path>
+                    </svg>
+                  </div>
+                )}
+              />
+            </div>
           </div>
         </form>
       </FormProvider>
